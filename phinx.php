95b5e77237c2f78d15f8490cec11e11d4b41ce49
Marco De Felice <marco.defelice@qwentes.it>
2024-07-11 20:06:34 +0200

    env for github_actions

--- phinx.php
+++ phinx.php
@@ -19,5 +19,15 @@ return [
             'charset'  => 'utf8',
             'port' => env('DB_PORT')
         ],
+        'github_actions' => [
+            'adapter'   => 'mysql',
+            'host' => env('DB_HOST'),
+            'name' => env('DB_DATABASE'),
+            'user' => env('DB_USERNAME'),
+            'pass' => env('DB_PASSWORD'),
+            'charset'  => 'utf8',
+            'port' => env('DB_PORT'),
+            'migrations' => 'resources/github_actions_migrations'
+        ],
     ],
 ];
