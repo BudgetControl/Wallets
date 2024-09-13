c8361ad7887e75b481d35de110cb95032a02b916
Marco De Felice <marco.defelice890@gmail.com>
2024-09-13 15:25:52 +0200

    config test

--- test.php
+++ test.php
@@ -2,6 +2,7 @@
 
 require_once __DIR__ . '/bootstrap/app.php';
 
+use Budgetcontrol\Library\Model\User;
 use Illuminate\Database\Capsule\Manager as DB;
 use Budgetcontrol\Wallet\Facade\Crypt;
 
@@ -14,5 +15,17 @@ foreach ($wallets as $wallet) {
     DB::table('wallets')
         ->where('id', $wallet->id)
         ->update(['name' => $walletName]);
+
+}
+
+$users = User::all();
+foreach ($users as $user) {
+    echo $user->name . PHP_EOL;
+
+    $userName = Crypt::encrypt($user->name);
+    
+    DB::table('users')
+        ->where('id', $user->id)
+        ->update(['name' => $userName]);
 
 }
