c75b47693380d43d13279f25008042ed9cffac2b
Marco De Felice <marco.defelice890@gmail.com>
2024-09-13 15:21:15 +0200

    added facade annotations

--- src/Facade/Crypt.php
+++ src/Facade/Crypt.php
@@ -5,6 +5,13 @@ namespace Budgetcontrol\Wallet\Facade;
 
 use Illuminate\Support\Facades\Facade;
 
+/**
+ * @method static string encrypt(string $value)
+ * @method static string decrypt(string $value)
+ * 
+ * @see \BudgetcontrolLibs\Crypt\Service\CryptableService
+ */
+
 final class Crypt extends Facade
 {
     protected static function getFacadeAccessor(): string
